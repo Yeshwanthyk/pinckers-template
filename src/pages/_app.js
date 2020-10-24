b017fa5342109e2fe15db5132a929af23c569888
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-10-23 20:05:04 -0400

    Add base packages

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import { ThemeProvider } from 'styled-components'
+import App from 'next/app';
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Nextjs Starter</title>
+        <link rel="icon" href="../src/assets/favicon.ico" />
+      </Head>
+
+			Hello
+
+    </div>
+  )
+}
