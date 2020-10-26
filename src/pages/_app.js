784f27a5a017a222f1c937351710281b84abfb75
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-10-26 13:31:08 -0400

    Add chakraUI to _app and _document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,25 @@
-import { ThemeProvider } from 'styled-components'
 import App from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import {
+  ThemeProvider as ChakraProvider,
+  CSSReset,
+  Box,
+} from '@chakra-ui/core';
+import { theme } from '@chakra-ui/core';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <>
+          <CSSReset />
+          <Box padding={8} maxWidth="80%" margin="auto">
+            <Component {...pageProps} />
+          </Box>
+        </>
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
