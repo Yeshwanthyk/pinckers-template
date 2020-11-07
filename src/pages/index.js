811c5d686e627e00b29d4201cbf04ec18735a86e
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-11-07 09:22:17 -0500

    Add inital layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
-import Head from 'next/head'
+import { Box, Flex, Link } from '@chakra-ui/core';
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <Box>
       <Head>
-        <title>Nextjs Starter</title>
+        <title>MP Portfolio</title>
         <link rel="icon" href="../src/assets/favicon.ico" />
       </Head>
 
-			Hello
-
-    </div>
-  )
+      <Flex>
+        <Flex flexBasis="10rem" flexGrow="1">
+          <Box as="nav">Side Nav</Box>
+        </Flex>
+        <Flex flexDirection="column" flexBasis="0" flexGrow="999" ml={8}>
+          <Flex>
+            <Box> Prev</Box>
+            <Box> | </Box>
+            <Box> Next</Box>
+          </Flex>
+          <Image src="/test_1.jpg" width="400px" height="600px" />
+        </Flex>
+      </Flex>
+    </Box>
+  );
 }
---
+++ src/pages/projects/[project].js
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/core';
+import React from 'react';
+
+const ProjectPage = () => {
+  return <Box>Images</Box>;
+};
+
+export default ProjectPage;
