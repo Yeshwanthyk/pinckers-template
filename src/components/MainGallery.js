69b49ec0da7b8c11d1b8d69f7bc4160ff3ef7544
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-11-07 22:30:41 -0500

    MainGallery component

---
+++ src/components/MainGallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+const MainGallery = () => {
+  return (
+    <>
+      <Image src="/briefly/test_1.jpg" width="400px" height="600px" />
+    </>
+  );
+};
+
+export default MainGallery;
