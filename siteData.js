0c1f25991954a2e6351fcff00482dc4e84683b88
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-11-07 20:10:10 -0500

    Add project data

---
+++ siteData.js
@@ -0,0 +1,13 @@
+const siteData = [
+  {
+    name: 'briefly',
+    desc: 'Family is a beautiful thing',
+    mainImage: 'main',
+  },
+  {
+    name: 'basement',
+    desc: 'No light for a year',
+  },
+];
+
+export default siteData;
