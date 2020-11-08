0c5888c82cf8b631f7947b1bd2fd38e5da195f7e
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-11-08 00:45:06 -0500

    Add logo

--- siteData.js
+++ siteData.js
@@ -1,12 +1,15 @@
 const siteData = [
   {
     name: 'briefly',
-    desc: 'Family is a beautiful thing',
+    desc: `Are you suggesting coconuts migrate? Oh and Jenkins, apparently your mother died this morning. It's not pining, it's passed on! This parrot is no more! It has ceased to be! It's expired and gone to meet its maker! This is a late parrot! It's a stiff! Bereft of life, it rests in peace! If you hadn't nailed it to the perch, it would be pushing up the daisies! It's metabolic processes are now history! He's off the twig! He's kicked the bucket, he's shuffled off the mortal coil, rung down the curtain and joined the choir invisible. This is an ex-parrot! Not necessarily. I could be arguing in my spare time.
+    `,
     mainImage: 'main',
+    imageNum: 3,
   },
   {
     name: 'basement',
     desc: 'No light for a year',
+    imageNum: 2,
   },
 ];
 
