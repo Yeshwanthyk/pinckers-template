f17eb56c2c6224143666adb728f293ff2484020f
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-12-21 18:53:15 -0500

    Add SEO using next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,25 @@
+import siteData from './siteData';
+
+const title = siteData.title;
+const description = siteData.description;
+const SEO = {
+  title,
+  description,
+  canonical: siteData.websiteUrl,
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: siteData.websiteUrl,
+    title,
+    description,
+    images: [
+      {
+        url: `${siteData.websiteUrl}${siteData.mainImage}`,
+        alt: title,
+        width: 1280,
+        height: 720,
+      },
+    ],
+  },
+};
+export default SEO;
